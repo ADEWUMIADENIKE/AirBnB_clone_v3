f62662d667e23ca1d0778843f3c7e2a9108874d7
ADEWUMIADENIKE <111264388+ADEWUMIADENIKE@users.noreply.github.com>
2023-05-01 20:55:58 -0700

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,60 +1,71 @@
 #!/usr/bin/python3
-"""
-    Flask route that returns json response
-"""
+""" users """
+from models import storage
 from api.v1.views import app_views
-from flask import abort, jsonify, request
-from models import storage, CNC
-from flasgger.utils import swag_from
+from flask import jsonify, abort, make_response
+from flask import request
+from models.user import User
 
 
-@app_views.route('/users/', methods=['GET', 'POST'])
-@swag_from('swagger_yaml/users_no_id.yml', methods=['GET', 'POST'])
-def users_no_id(user_id=None):
-    """
-        users route that handles http requests with no ID given
-    """
-
-    if request.method == 'GET':
-        all_users = storage.all('User')
-        all_users = [obj.to_json() for obj in all_users.values()]
-        return jsonify(all_users)
-
-    if request.method == 'POST':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        if req_json.get('email') is None:
-            abort(400, 'Missing email')
-        if req_json.get('password') is None:
-            abort(400, 'Missing password')
-        User = CNC.get('User')
-        new_object = User(**req_json)
-        new_object.save()
-        return jsonify(new_object.to_json()), 201
+@app_views.route('/users',  methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<string:user_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_users(user_id=None):
+    """ get user(s) information """
+    if user_id is None:
+        list_users = []
+        users = storage.all("User").values()
+        for user in users:
+            list_users.append(user.to_dict())
+        return jsonify(list_users)
+    else:
+        user = storage.get("User", user_id)
+        if user is None:
+            abort(404)
+        else:
+            return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'])
-@swag_from('swagger_yaml/users_id.yml', methods=['GET', 'DELETE', 'PUT'])
-def user_with_id(user_id=None):
-    """
-        users route that handles http requests with ID given
-    """
-    user_obj = storage.get('User', user_id)
-    if user_obj is None:
-        abort(404, 'Not found')
+@app_views.route('/users/<string:user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_User(user_id):
+    """ delete a User """
+    user = storage.get("User", user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return make_response(jsonify({}), 200)
 
-    if request.method == 'GET':
-        return jsonify(user_obj.to_json())
 
-    if request.method == 'DELETE':
-        user_obj.delete()
-        del user_obj
-        return jsonify({}), 200
+@app_views.route('/users/', methods=['POST'], strict_slashes=False)
+def create_User():
+    """ create a User """
+    req = request.get_json()
+    if req is None:
+        abort(400, "Not a JSON")
+    if 'email' not in req:
+        abort(400, "Missing email")
+    if 'password' not in req:
+        abort(400, "Missing password")
+    new_user = User(email=req['email'], password=req['password'])
+    new_user.save()
+    return make_response(jsonify((new_user.to_dict())), 201)
 
-    if request.method == 'PUT':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        user_obj.bm_update(req_json)
-        return jsonify(user_obj.to_json()), 200
+
+@app_views.route('/users/<user_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_User(user_id):
+    """ update a User """
+    user = storage.get("User", user_id)
+    if user is None:
+        abort(404)
+    req = request.get_json()
+    if req is None:
+        abort(400, "Not a JSON")
+    ignore = ['id', 'created_at', 'updated_at', 'email']
+    for attribute, value in req.items():
+        if attribute not in ignore:
+            setattr(user, attribute, value)
+    user.save()
+    return make_response(jsonify((user.to_dict())), 200)
